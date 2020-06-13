d92a0f9d10cadf7185ae8c8bd533b904e915d3db
KumakuraToyotsugu <tokkun.nga-.0423@outlook.jp>
2020-06-13 09:08:03 +0900

    戦闘画面途中　召喚ゲージ完成

--- RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Battle/BattleController.cs
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Battle/BattleController.cs
@@ -8,8 +8,14 @@ public class BattleController : MonoBehaviour
 {
     [SerializeField] private float gameTime = 90;   // 残り時間
     [SerializeField] private Text timeText;
+    [SerializeField] private Image summonGage;
 
-    private int summonGage = 5;     // 召喚ゲージ　: 最大は10
+    private float summonGageVal = 5.0f;     // 召喚ゲージ　: 最大は10
+    public float SummonGageVal
+    {
+        get { return summonGageVal; }
+        set { summonGageVal = value; }
+    }
 
     void Start()
     {
@@ -20,6 +26,25 @@ public class BattleController : MonoBehaviour
     {
         gameTime -= Time.deltaTime;
         timeText.text = gameTime.ToString("00");
+
+        summonGageVal = Mathf.Clamp(summonGageVal + Time.deltaTime / 3, 0.0f, 10.0f);
+
+        // デバッグ用
+        //-----------------------------------------------------------------------------
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            summonGageVal = Mathf.Clamp(summonGageVal + 1.0f, 0.0f, 10.0f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            summonGageVal = Mathf.Clamp(summonGageVal - 1.0f, 0.0f, 10.0f);
+        }
+
+        //-----------------------------------------------------------------------------
+
+        summonGage.fillAmount = summonGageVal / 10.0f;
 
         #region シーン遷移
         // 条件は後々変更する
---
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Character/CharacterController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class CharacterController : CharacterInfo
+{
+    void Start()
+    {
+        AttackMethod(myAttackMethod);
+        AttackRange(myAttackRange);
+        AttackSpeed(myAttackSpeed);
+        CharaCharacteristic(myCharacteristic);
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 攻撃方法
+    /// </summary>
+    /// <param name="atkMethod">攻撃方法</param>
+    public void AttackMethod(attackMethod atkMethod)
+    {
+        switch (atkMethod)
+        {
+            case attackMethod.shortDistance:
+                // 攻撃方法　小設定
+                break;
+
+            case attackMethod.mediumDistance:
+                // 攻撃方法　中設定
+                break;
+
+            case attackMethod.longDistance:
+                // 攻撃方法　大設定
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 攻撃範囲
+    /// </summary>
+    /// <param name="atkRebge">攻撃範囲</param>
+    public void AttackRange(attackRange atkRebge)
+    {
+        switch (atkRebge)
+        {
+            case attackRange.small:
+                // 攻撃範囲　小設定
+                break;
+
+            case attackRange.large:
+                // 攻撃範囲　大設定
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 攻撃速度
+    /// </summary>
+    /// <param name="atkSpeed">攻撃速度</param>
+    public void AttackSpeed(attackSpeed atkSpeed)
+    {
+        switch (atkSpeed)
+        {
+            case attackSpeed.slow:
+                // 攻撃速度　遅い設定
+                break;
+
+            case attackSpeed.usually:
+                // 攻撃速度　普通設定
+                break;
+
+            case attackSpeed.fast:
+                // 攻撃速度　速い設定
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 特徴
+    /// </summary>
+    /// <param name="atkCharacteristic">特徴</param>
+    public void CharaCharacteristic(characteristic atkCharacteristic)
+    {
+        switch (atkCharacteristic)
+        {
+            case characteristic.none:
+                // 特徴　無し設定
+                break;
+            
+            case characteristic.quickness:
+                // 特徴　俊足設定
+                break;
+
+            case characteristic.ironWall:
+                // 特徴　鉄壁設定
+                break;
+            
+            case characteristic.covert:
+                // 特徴　隠密設定
+                break;
+            
+            case characteristic.explosion:
+                // 特徴　爆発設定
+                break;
+            
+            case characteristic.electricShock:
+                // 特徴　感電設定
+                break;
+        }
+    }
+
+
+}
