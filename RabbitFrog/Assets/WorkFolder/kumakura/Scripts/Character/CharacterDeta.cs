5dee0150cf1809c14cf084205638bb2853a449b9
KumakuraToyotsugu <tokkun.nga-.0423@outlook.jp>
2020-06-24 16:08:40 +0900

    クラス構成の練り直し、継承関係の見直し

---
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Character/CharacterDeta.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterDeta
+{
+    public string name = "";
+    public int hp;
+    public int maxHp;
+    public int power;
+}
---
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Character/Unit/InfantryCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfantryCharacter : Character
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void Death()
+    {
+        base.Death();
+        gameObject.SetActive(false);
+    }
+}
