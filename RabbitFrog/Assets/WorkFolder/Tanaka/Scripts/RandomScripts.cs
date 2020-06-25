c31b7d0d006d1897c340d407a4f2263f9726b545
TanakaAmi <amiamico0129@gmail.com>
2020-06-26 08:47:55 +0900

    コミット

---
+++ RabbitFrog/Assets/WorkFolder/Tanaka/Scripts/RandomScripts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 練習------------
+    static void Test()
+    {
+        System.Random r1 = new System.Random();
+        Console.WriteLine(r1.Next(0, 3));
+
+        System.Random r2 = new System.Random();
+        Console.WriteLine(r2.Next(1, 4));
+    }
+}
