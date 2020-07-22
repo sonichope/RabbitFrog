45fd0e35c73d6c67a7619eb5bdf431eabbe363d1
KumakuraToyotsugu <tokkun.nga-.0423@outlook.jp>
2020-07-22 18:28:29 +0900

    とりあえずシナリオのテキスト送りは完成

--- RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Scenario/ScenarioTextControl.cs
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Scenario/ScenarioTextControl.cs
@@ -7,26 +7,67 @@ public class ScenarioTextControl : MonoBehaviour
 {
     [SerializeField] private string[] sentence; // シナリオを格納する箱
     [SerializeField] private Text uiText;       // 
-    private int nowLineNum = 0;                 // 現在の行番号
+
+    [SerializeField, Range(0.01f, 0.3f)]
+    float intervalForCharacterDisplay = 0.05f;  // １文字の表示にかかる時間
+    
+    private int currentLine = 0;
+    private string currentText = string.Empty;  // 現在の文字列
+    private float timeUntilDisplay = 0;         // 表示にかかる時間
+    private float timeElapsed = 1;              // 文字列の表示を開始した時間
+    private int lastUpdateCharacter = -1;       // 表示中の文字列
+
+    // 文字の表示が完了しているかどうか
+    public bool IsCompleteDisplayText
+    {
+        get { return Time.time > timeElapsed + timeUntilDisplay; }
+    }
 
 
     void Start()
     {
-        TextUpdate();
+        SetNextLine();
     }
 
     void Update()
     {
-        // 現在の行番号が最後まで行ってない状態でクリックすると、テキストを更新する
-        if (nowLineNum < sentence.Length && Input.GetMouseButtonDown(0))
+        if (IsCompleteDisplayText)
         {
-            TextUpdate();
+            if (currentLine < sentence.Length && Input.GetMouseButtonDown(0))
+            {
+                SetNextLine();
+            }
+        }
+        else
+        {
+            // 完了していない文字をすべて表示する
+            if (Input.GetMouseButtonUp(0))
+            {
+                timeUntilDisplay = 0;
+            }
+        }
+
+        // クリックから経過した時間が想定表示時間の何％か確認し、表示文字数を出す
+        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
+
+        // 表示文字数が前回の表示文字数と異なるならテキストを更新する
+        if (displayCharacterCount != lastUpdateCharacter)
+        {
+            uiText.text = currentText.Substring(0, displayCharacterCount);
+            lastUpdateCharacter = displayCharacterCount;
         }
     }
 
-    private void TextUpdate()
+    private void SetNextLine()
     {
-        uiText.text = sentence[nowLineNum];     // 現在の行のシナリオを表示
-        nowLineNum++;                           // 次の行番号へ
+        currentText = sentence[currentLine];
+        currentLine++;
+
+        // 想定表示時間と現在の時刻をキャッシュ
+        timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+        timeElapsed = Time.time;
+
+        // 文字カウントを初期化
+        lastUpdateCharacter = -1;
     }
 }
