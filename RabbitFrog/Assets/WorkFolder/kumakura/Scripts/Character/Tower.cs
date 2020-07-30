76580b701430eee35c0e5e43a45fc67a67157fb0
KumakuraToyotsugu <tokkun.nga-.0423@outlook.jp>
2020-07-30 17:13:04 +0900

    TowerPrefab,TowerScriptの初期構成　α版まであと少し、ぴえん

---
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Character/Tower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : CharacterBase
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 攻撃
+    /// </summary>
+    public override void Attack()
+    {
+        
+    }
+    
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    public override void Death()
+    {
+        
+    }
+}
