365baeb903dd95bea62659bcaadd7fe9615e63ac
KumakuraToyotsugu <tokkun.nga-.0423@outlook.jp>
2020-08-02 23:27:47 +0900

    Editor拡張が動いてくれんかった,,,探さないでください,,,

--- RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Character/CreateEnemyPosition.cs
+++ RabbitFrog/Assets/WorkFolder/kumakura/Scripts/Character/CreateEnemyPosition.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class CreateEnemyPosition : MonoBehaviour
 {
-    void Start()
+    public Texture areaTex;
+    public GUIStyle style;
+    public Vector3 pos = Vector3.zero;
+    public Quaternion rot = Quaternion.identity;
+    public Vector3 scale = Vector3.one;
+    public Transform[] points;
+    public Vector3 lookAtPoint;
+
+    public void DataUpdate()
     {
-        
+        transform.rotation = rot;
+        transform.position = pos;
+        transform.localScale = scale;
     }
 
-    void Update()
+    public void LookAtUpdate()
     {
-        
+        transform.LookAt(lookAtPoint);
     }
 }
