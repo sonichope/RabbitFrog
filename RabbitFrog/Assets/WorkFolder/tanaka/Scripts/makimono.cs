12599742a1688c7ec9a6776b0f03e2cf83e9ced0
TanakaAmi <amiamico0129@gmail.com>
2020-08-07 18:42:44 +0900

    komitto

---
+++ RabbitFrog/Assets/WorkFolder/tanaka/Scripts/makimono.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makimono : MonoBehaviour
+{
+    // [SerializeField] private GameObject makimono01;
+    // Start is called before the first frame update
+    private bool vecFlag = false;
+    void Start()
+    {
+        StartCoroutine("SampleCoroutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Test();
+    }
+
+    IEnumerator SampleCoroutine()
+    {
+        if (vecFlag == true)
+            yield return new WaitForSeconds(5.0f);
+        
+    }
+
+    void Test()
+    {
+        if (vecFlag) { return; }
+        vecFlag = true;
+        Transform myTransform = transform;
+        Vector2 pos = myTransform.position;
+        pos.x += -0.01f;
+        pos.y += 0;
+        myTransform.position = pos;
+    }
+}
