786f62d3f14871b08fe38e11c2938e771a29afd2
miyatatukasa <miyata2001@gmail.com>
2020-10-09 09:55:42 +0900

    リタイア関係

---
+++ RabbitFrog/Assets/WorkFolder/miyata/Scripts/TaitleBackCon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaitleBackCon : MonoBehaviour
+{
+    [SerializeField]
+    GameObject TitleChoicesCon;
+    [SerializeField]
+    GameObject TitleChoicesCon2;
+    [SerializeField]
+    GameObject TitleChoicesCon3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void active()
+    {
+        TitleChoicesCon.SetActive(true);
+        TitleChoicesCon2.SetActive(false);
+    }
+
+    public void optionBack()
+    {
+        TitleChoicesCon2.SetActive(false);
+        TitleChoicesCon3.SetActive(true);
+    }
+
+
+}
