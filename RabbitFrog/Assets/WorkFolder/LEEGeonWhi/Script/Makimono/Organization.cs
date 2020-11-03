547301c8ff98d1b34c7214c8d1d1667bb5c40a63
congibab <engljapen2@gmail.com>
2020-11-04 02:05:21 +0900

    Option 巻物Animation追加

---
+++ RabbitFrog/Assets/WorkFolder/LEEGeonWhi/Script/Makimono/Organization.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organization : MonoBehaviour
+{
+    RectTransform rt;
+   
+    Vector2 default_Size;
+    Vector2 Change_Size;
+
+    Vector2 Open_Pos;
+    Vector2 Close_Pos;
+
+    bool is_Click = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+        default_Size = rt.sizeDelta;
+        Change_Size = rt.sizeDelta;
+        Change_Size.y = rt.sizeDelta.y + 100;
+
+
+    }
+
+    public void Open_Close()
+    {
+        is_Click = !is_Click;
+        if(is_Click)
+        {
+            rt.sizeDelta = Change_Size;
+        }
+
+        else
+        {
+            rt.sizeDelta = default_Size;
+        }
+    }
+
+    public void Open()
+    {
+        rt.sizeDelta = Change_Size;
+    }
+
+    public void Close()
+    {
+        rt.sizeDelta = default_Size;
+    }
+}
---
+++ RabbitFrog/Assets/WorkFolder/LEEGeonWhi/Script/Makimono/OrganizationMask.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrganizationMask : MonoBehaviour
+{
+    public Image image;
+   public bool start_anime = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (image.fillAmount <= 1 && start_anime)
+        {
+            image.fillAmount += Time.deltaTime;
+        }
+
+        if (image.fillAmount >= 0 && !start_anime)
+        {
+            image.fillAmount -= Time.deltaTime;
+        }
+
+
+    }
+
+    public void Run_animation()
+    {
+        //start_anime = !start_anime;
+        start_anime = true;
+    }
+
+    //public IEnumerator test1()
+    //{
+    //    yield return new WaitForSeconds(Time.deltaTime);
+    //    while(image.fillAmount <= 1)
+    //    {
+    //        image.fillAmount += Time.deltaTime;
+
+    //    }
+    //}
+}
