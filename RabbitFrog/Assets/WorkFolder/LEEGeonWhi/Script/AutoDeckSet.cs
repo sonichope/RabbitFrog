02c1522f4552d63c9a120ac520a70f0918ee2b1e
KumakuraToyotsugu <tokkun.nga-.0423@ezweb.ne.jp>
2020-11-20 18:54:42 +0900

    おススメ編成の元完成

--- RabbitFrog/Assets/WorkFolder/LEEGeonWhi/Script/AutoDeckSet.cs
+++ RabbitFrog/Assets/WorkFolder/LEEGeonWhi/Script/AutoDeckSet.cs
@@ -5,18 +5,22 @@ using UnityEngine.UI;
 
 public class AutoDeckSet : MonoBehaviour
 {
-    [SerializeField] private Image[] deckImage = new Image[8];
-    [SerializeField] private DeckObject[] deckObjects = new DeckObject[8];
+    [SerializeField] private GameObject[] deck = new GameObject[8];
+    //[SerializeField] private Image[] deckImage = new Image[8];
+    //[SerializeField] private DeckObject[] deckObjects = new DeckObject[8];
     [SerializeField, Header("自動セットしたいカード")] private CardPoolObject[] cardPoolObjects = new CardPoolObject[8];
 
     public void AutoSetDeck()
     {
-        for(int i = 0; i < deckObjects.Length; i++)
+        for(int i = 0; i < deck.Length; i++)
         {
-            deckObjects[i].cardPoolObject = cardPoolObjects[i];
-            deckImage[i].sprite = deckObjects[i].cardPoolObject.character.image;
+            deck[i].GetComponent<DeckObject>().cardPoolObject = cardPoolObjects[i];
+            //deckObjects[i].cardPoolObject = cardPoolObjects[i];
+            deck[i].GetComponent<Image>().sprite = deck[i].GetComponent<DeckObject>().cardPoolObject.character.image;
+            //deckImage[i].sprite = deckObjects[i].cardPoolObject.character.image;
             //2020/10/25 bug修正　イゴンヒ
-            deckObjects[i].GetComponent<DeckObject>().nowSprite = deckImage[i].sprite;
+            //deckObjects[i].GetComponent<DeckObject>().nowSprite = deckImage[i].sprite;
+            deck[i].GetComponent<DeckObject>().nowSprite = deck[i].GetComponent<Image>().sprite;
         }
     }
 }
